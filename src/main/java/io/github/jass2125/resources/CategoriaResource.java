8acc958b0d32d4aaede1d49af864e1d4a5194f57
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-05-06 22:08:36 -0300

    testando primeira classe de domínio

--- src/main/java/io/github/jass2125/resources/CategoriaResource.java
+++ src/main/java/io/github/jass2125/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package io.github.jass2125.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jass2125.domains.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1L, "Informática");
+		Categoria c2 = new Categoria(2L, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
 }
