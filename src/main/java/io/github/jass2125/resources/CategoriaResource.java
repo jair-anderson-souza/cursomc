6a1a09ac626964793640acf9b92129dd97d8312e
Anderson Souza <jair.anderson@alfaconsultoria.digital>
2018-05-11 15:27:41 -0300

    adicionando pageamento no recurso de Categoria

--- src/main/java/io/github/jass2125/resources/CategoriaResource.java
+++ src/main/java/io/github/jass2125/resources/CategoriaResource.java
@@ -1,15 +1,15 @@
 package io.github.jass2125.resources;
 
 import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -51,9 +51,13 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<?> buscarTodos() {
-		List<Categoria> cat = catService.findAll();
-		List<CategoriaDTO> listaDeDto = cat.parallelStream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+	public ResponseEntity<?> buscarTodos(
+			@RequestParam(name = "page", defaultValue = "0") Integer page,
+			@RequestParam(name = "linesPerPage", defaultValue = "1") Integer linesPerPage,
+			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> categoriaPageable = catService.searchPageable(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listaDeDto = categoriaPageable.map(categoria -> new CategoriaDTO(categoria));
 		return ResponseEntity.ok().body(listaDeDto);
 	}
 }
--- src/main/java/io/github/jass2125/services/CategoriaService.java
+++ src/main/java/io/github/jass2125/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import io.github.jass2125.domains.Categoria;
@@ -41,5 +44,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> searchPageable(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageable = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageable);
 	}
 }
